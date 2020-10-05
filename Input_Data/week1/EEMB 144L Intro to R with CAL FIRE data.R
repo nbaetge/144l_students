5468f42141d6392153e4aa5c9bacf85f808bc150
Kerri Luttrell <kerri@Kerris-MacBook-Pro.local>
2020-10-05 15:45:27 -0700

    first script for class on 10/5/2020

---
+++ Input_Data/week1/EEMB 144L Intro to R with CAL FIRE data.R
@@ -0,0 +1,25 @@
+##### EEMB 144L Intro to R with CAL FIRE data #####
+#Kerri Luttrell
+#10/5/2020
+
+#installing packages
+
+#collection of useful packages
+install.packages("tidyverse")
+library(tidyverse)
+#way to import data
+install.packages("readxl")
+library(readxl)
+
+
+#### Load Data ####
+
+
+
+# .xlsx files can have multiple pages
+
+excel_sheets("2013_2019_CALFIRE_Redbook.xlsx")
+
+calfire.metadata <- read_excel("2013_2019_CALFIRE_Redbook.xlsx")
+
+
